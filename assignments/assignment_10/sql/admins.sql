c32eabe8f3800ddc6aa12c3d91494b39db335f86
hvandecar <122235170+hvandecar@users.noreply.github.com>
2023-12-12 00:29:53 -0500

    Delete assignments/assignment_10/sql directory

--- assignments/assignment_10/sql/admins.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE finalAdmins (
-  id int NOT NULL AUTO_INCREMENT,
-  name char(100) NOT NULL,
-  email char(100) NOT NULL,
-  password char(250) NOT NULL,
-  status char(100) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE = InnoDB;
